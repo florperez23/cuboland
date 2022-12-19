19dd6cb934b2fe459b2d825d80a2511fc45a2605
Yesica <nava.yesik@gmail.com>
2022-12-19 13:04:54 -0600

    eliminar producto

--- sistema/eliminar_producto.php
+++ sistema/eliminar_producto.php
@@ -3,7 +3,7 @@
 if (!empty($_GET['id'])) {
     require("../conexion.php");
     $codproducto = $_GET['id'];
-    $query_delete = mysqli_query($conexion, "DELETE FROM producto WHERE codproducto = $codproducto");
+    $query_delete = mysqli_query($conexion, "DELETE FROM producto WHERE codproducto = '$codproducto'");
     historia('Se elimino un producto '.$codproducto);
     mysqli_close($conexion);
     mensajeicono('Se ha eliminado con éxito el producto!', 'lista_productos.php','','exito');
